6e27e1e81594376c4a3bd3ce1fd3fe1b7fdd6452
rohinmeduri <rohinmeduri@gmail.com>
2018-02-13 23:22:13 -0800

    added primitive multiplayer

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Movement : MonoBehaviour {
+public class Movement : NetworkBehaviour {
 
     public bool jumpable = true;
 
@@ -14,6 +15,10 @@ public class Movement : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(10 * Input.GetAxis("Horizontal"), gameObject.GetComponent<Rigidbody2D>().velocity.y);
         if (Input.GetAxis("Jump") > 0 && jumpable)
         {
