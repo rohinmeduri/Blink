61605da2a0087bb118d124f37c24984b798110c3
Daniel Liu <hi.daniel.liu@gmail.com>
2018-02-13 22:36:01 -0800

    rudimentary controls

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public bool jumpable = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(10 * Input.GetAxis("Horizontal"), gameObject.GetComponent<Rigidbody2D>().velocity.y);
+        if (Input.GetAxis("Jump") > 0 && jumpable)
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(gameObject.GetComponent<Rigidbody2D>().velocity.x, 10);
+            jumpable = false;
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Ground")
+        {
+            jumpable = true;
+        }
+    }
+}
