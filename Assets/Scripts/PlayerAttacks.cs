2fa16201e665176dbe4da11fb8923303897f5a7e
rohinmeduri <rohinmeduri@gmail.com>
2018-02-18 16:27:43 -0800

    added glory prefab to player

--- Assets/Scripts/PlayerAttacks.cs
+++ Assets/Scripts/PlayerAttacks.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
-public class PlayerAttacks : NetworkBehaviour {
-    // Update is called once per frame
+public class PlayerAttacks : NetworkBehaviour
+{
     private bool canAttack = true;
     private bool attackButtonHeld = false;
     public GameObject player;
@@ -13,10 +14,48 @@ public class PlayerAttacks : NetworkBehaviour {
     public float playerWidth;
     public float playerHeight;
     public LayerMask mask;
-    
-    void FixedUpdate () {
-        //Debug.Log(attackButtonHeld);
+    public GameObject gloryPrefab;
+    public GameObject glory;
+    private int gloryWaitFrames = 2;
+    private int gloryWaitedFrames = 0;
 
+    [SyncVar(hook = "OnChangeGlory")]
+    public float numGlory = 0;
+
+    public Slider glorySlider;
+    public float baseGloryGain;
+
+    public void createMeter()
+    {
+        glory = (GameObject)Instantiate(gloryPrefab);
+        var canvas = GameObject.Find("Canvas");
+        RectTransform gloryTransform = glory.GetComponent<RectTransform>();
+        gloryTransform.SetParent(canvas.transform);
+        if (!hasAuthority)
+        {
+            gloryTransform.anchorMin = new Vector2(1, 1);
+            gloryTransform.anchorMax = new Vector2(1, 1);
+            gloryTransform.pivot = new Vector2(1, 1);
+            gloryTransform.anchoredPosition = new Vector3(-100, 0, 0);
+        }
+        else
+        {
+            gloryTransform.anchoredPosition = new Vector3(0, 0, 0);
+        }
+        glorySlider = glory.transform.Find("Slider").gameObject.GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        gloryWaitedFrames++;
+        if (gloryWaitedFrames == gloryWaitFrames)
+        {
+            createMeter();
+        }
+    }
+
+    void FixedUpdate()
+    {
         if (!hasAuthority)
         {
             return;
@@ -27,7 +66,7 @@ public class PlayerAttacks : NetworkBehaviour {
         if (!canAttack)
         {
             waitedFrames++;
-            if(waitedFrames == waitFrames)
+            if (waitedFrames == waitFrames)
             {
                 canAttack = true;
                 waitedFrames = 0;
@@ -46,7 +85,7 @@ public class PlayerAttacks : NetworkBehaviour {
             if (attackButtonHeld && canAttack)
             {
                 //cancel attacker's momentum
-                player.GetComponent<Rigidbody2D>().velocity = new Vector2 (0, 0);
+                player.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
 
                 //determine horizontal component of attack's direction
                 float horizontalDirection;
@@ -66,7 +105,7 @@ public class PlayerAttacks : NetworkBehaviour {
                 {
                     horizontalDirection = Input.GetAxis("Horizontal");
                 }
-                
+
                 //raycast to see if someone is hit with the attack - mask out attacker's layer
                 Vector2 direction = new Vector2(horizontalDirection, Input.GetAxis("Vertical"));
                 direction.Normalize();
@@ -75,6 +114,7 @@ public class PlayerAttacks : NetworkBehaviour {
                 RaycastHit2D hit = Physics2D.Raycast(origin: origin, direction: direction, distance: attackRadius, layerMask: mask.value);
                 if (hit.rigidbody != null)
                 {
+                    CmdChangeGlory();
                     CmdKnockback(hit.rigidbody.gameObject, direction);
                 }
 
@@ -85,7 +125,24 @@ public class PlayerAttacks : NetworkBehaviour {
             //keep track that attack button wasn't held during this frame
             attackButtonHeld = false;
         }
-	}
+    }
+
+    [Command]
+    void CmdChangeGlory()
+    {
+        numGlory += baseGloryGain;
+    }
+
+    void OnChangeGlory(float glory)
+    {
+        numGlory = glory;
+        if (glorySlider != null)
+        {
+            glorySlider.value = glory;
+        }
+
+        Debug.Log("change received glory is now: " + glory);
+    }
 
     //apply knockback to attack recipient on the server
     [Command]
@@ -97,10 +154,9 @@ public class PlayerAttacks : NetworkBehaviour {
             go.GetComponent<PlayerMovement>().hitStun();
         }
         RpcKnockback(go, dir);
-
     }
 
-    //apply knowckback to attack recipients on the clients
+    //apply knockback to attack recipients on the clients
     [ClientRpc]
     void RpcKnockback(GameObject go, Vector2 dir)
     {
@@ -111,3 +167,4 @@ public class PlayerAttacks : NetworkBehaviour {
         }
     }
 }
+
