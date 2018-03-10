88cf90aad612928844eb742090336c4447d2c95e
Alan Tao <alantao2000@hotmail.com>
2018-03-09 22:36:52 -0800

    f

---
+++ Assets/Scripts/Blinks.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blinks : MonoBehaviour{
+
+	private bool gloryFull;
+
+	public Blinks(float glory){
+		if (glory != 100) {
+			gloryFull = false;
+		} 
+		else {
+			gloryFull = true;
+		}
+			
+	}
+
+	public bool canBlink(){
+		return gloryFull;
+	}
+
+	public void teleport(){
+		
+	}
+		
+}
