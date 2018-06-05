209c28eb2dddaa9c85f058253557f47f4e060cce
Simon Liu <alantao2000@hotmail.com>
2018-06-04 21:38:26 -0700

    added another tutorial script

---
+++ Assets/Scripts/TutorialStepManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class TutorialStepManager : MonoBehaviour {
+
+    public int tutorialStep = 0;
+
+    protected string[] tutorialText = new string[11];
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void read() {
+        StreamReader textInput = new StreamReader("Assets\\Resources\\tutorialText.txt");
+    }
+}
