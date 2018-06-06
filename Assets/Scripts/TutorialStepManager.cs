310d1b848ec80992b72516703ab72f52caebb06b
Alan Tao <alantao2000@hotmail.com>
2018-06-06 00:20:13 -0700

    framework for tutorial

--- Assets/Scripts/TutorialStepManager.cs
+++ Assets/Scripts/TutorialStepManager.cs
@@ -6,33 +6,77 @@ using System.Text.RegularExpressions;
 
 public class TutorialStepManager : MonoBehaviour {
 
-    public int tutorialStep = 0;
+    public GameObject airDummy;
+    public GameObject reversalAI;
 
-    protected string[] tutorialText;
-	// Use this for initialization
-	void Start () {
-        tutorialText = read();
-        for (int i = 0; i < tutorialText.Length; i++){
-            Debug.Log(tutorialText[i]);
-        }
-	}
+    public int tutorialStep = 0; //current step tutorial is on
+
+    private bool nextStep = false; //is the tutorial moving on to the next step
+
+    protected string[] tutorialText; //will contain the tutorial instructions
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Start () {
+
+        tutorialText = read();
+
+        /*for (int i = 0; i < tutorialText.Length; i++){
+            Debug.Log(tutorialText[i]);
+        }*/
 	}
 
-    string[] read() {
+    void Update(){
+        /*if(nextStep){
+            switch (tutorialStep)
+            {
+                case 1:
+                    uAir();
+                    break;
+                case 2:
+                    dAir();
+                    break;
+                case 3:
+                    blink();
+                case 4:
+                    reversal();
+                case 5:
+                    super();
+                default:
+                    break;
+            }
+        }*/
+    }
 
-        FileStream fs = new FileStream("Assets\\Resources\\TutorialText.txt", FileMode.Open);
-        string content = "";
-        using (StreamReader fReader = new StreamReader(fs, true))
+    string[] read() { //reads text file and organizes each line into a string array
+
+        FileStream fStream = new FileStream("Assets/Resources/TutorialText.txt", FileMode.Open);
+        string instructions = "";
+        using (StreamReader fReader = new StreamReader(fStream, true))
         {
-            content = fReader.ReadToEnd();
+            instructions = fReader.ReadToEnd();
         }
 
-        string [] result = Regex.Split(content, "\r\n?|\n", RegexOptions.Singleline); //checks for "enter" in the text file, and splits string accordingly
+        string [] result = Regex.Split(instructions, "\r\n?|\n", RegexOptions.Singleline); //checks for "enter" in the text file, and splits string accordingly
 
         return result; 
+    }
+
+    void uAir(){
+        
+    }
+
+    void dAir(){
+        
+    }
+
+    void blink(){
+        
+    }
+
+    void reversal(){
+        
+    }
+
+    void super(){
+        
     }
 }
