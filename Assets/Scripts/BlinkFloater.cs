1cb4b487f74e07778b189aa2ff2a8f9feb453b1d
rohinmeduri <rohinmeduri@gmail.com>
2018-06-06 16:14:09 -0700

    made blink title float

--- Assets/Scripts/BlinkFloater.cs
+++ Assets/Scripts/BlinkFloater.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BlinkFloater : MonoBehaviour {
-
+    private float timer = 0f;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +11,9 @@ public class BlinkFloater : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        timer += Time.deltaTime;
+        GetComponent<RectTransform>().position = new Vector3(GetComponent<RectTransform>().position.x, 
+            GetComponent<RectTransform>().position.y + 0.3f * Mathf.Sin(timer * 1.15f));
+
+    }
 }
