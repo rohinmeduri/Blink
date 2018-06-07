3aa71098d251c9f3ca0486abcca7c2ad503b9ef5
Daniel Liu <hi.daniel.liu@gmail.com>
2018-06-07 13:34:44 -0700

    added cursor click sound

---
+++ Assets/Scripts/CursorSound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorSound : MonoBehaviour {
+
+    private bool created = false;
+    public AudioClip regularClick;
+    public AudioClip backClick;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (created) {
+            Destroy(gameObject);
+        }
+        created = true;
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void playRegular()
+    {
+        GetComponent<AudioSource>().PlayOneShot(regularClick);
+    }
+    public void playBack()
+    {
+        GetComponent<AudioSource>().PlayOneShot(backClick);
+    }
+}
--- Assets/Scripts/LoadSceneOnClick.cs
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -8,7 +8,6 @@ public class LoadSceneOnClick : MonoBehaviour
 
     public void LoadByIndex(int sceneIndex)
     {
-        Debug.Log("button click");
         SceneManager.LoadScene(sceneIndex);
     }
 }
