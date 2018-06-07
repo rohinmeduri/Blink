468fd12c924c597507a3820f1a6f142df3fe1724
Simon Liu <alantao2000@hotmail.com>
2018-06-06 17:11:32 -0700

    framework for TutorialStepManager established

--- Assets/Scripts/TutorialStepManager.cs
+++ Assets/Scripts/TutorialStepManager.cs
@@ -8,26 +8,33 @@ public class TutorialStepManager : MonoBehaviour {
 
     public GameObject airDummy;
     public GameObject reversalAI;
+    public GameObject localPlayer = GameObject.Find("Local Player");
 
     public int tutorialStep = 0; //current step tutorial is on
+    public int line = 0;
 
     private bool nextStep = false; //is the tutorial moving on to the next step
-
-    protected string[] tutorialText; //will contain the tutorial instructions
+    private string text; 
+    private string[] tutorialText; //will contain the tutorial instructions
 	
 	void Start () {
 
         tutorialText = read();
 
-        /*for (int i = 0; i < tutorialText.Length; i++){
+        for (int i = 0; i < tutorialText.Length; i++){
             Debug.Log(tutorialText[i]);
-        }*/
+        }
+        intro();
 	}
 
     void Update(){
-        /*if(nextStep){
+        if (nextStep)
+        {
             switch (tutorialStep)
             {
+                case 0:
+                    intro();
+                    break;
                 case 1:
                     uAir();
                     break;
@@ -36,14 +43,17 @@ public class TutorialStepManager : MonoBehaviour {
                     break;
                 case 3:
                     blink();
+                    break;
                 case 4:
                     reversal();
+                    break;
                 case 5:
                     super();
+                    break;
                 default:
                     break;
             }
-        }*/
+        }
     }
 
     string[] read() { //reads text file and organizes each line into a string array
@@ -60,18 +70,65 @@ public class TutorialStepManager : MonoBehaviour {
         return result; 
     }
 
-    bool getNextStep() {
+    public bool getNextStep() {
         return nextStep;
     }
 
-    void setNextStep(bool next)
+    public void setNextStep(bool next)
     {
         nextStep = next;
     }
 
-    int getTutorialStep()
+    public int getTutorialStep()
     {
         return tutorialStep;
     }
+
+    void intro() {
+        while (Input.GetAxisRaw(("Jump" + gameObject.GetComponent<LocalPlayerScript>().getControllerID())) == 0) {
+            displayLine(line);
+        }
+        while (Input.GetAxisRaw(("Jump" + gameObject.GetComponent<LocalPlayerScript>().getControllerID())) == 0)
+        {
+            displayLine(line);
+        }
+        while (Input.GetAxisRaw(("Jump" + gameObject.GetComponent<LocalPlayerScript>().getControllerID())) == 0)
+        {
+            displayLine(line);
+        }
+    }
+
+    void uAir()
+    {
+        
+        if (Input.GetAxisRaw(("Attack" + gameObject.GetComponent<LocalPlayerScript>().getControllerID())) != 0 && Input.GetAxisRaw(("Vertical" + gameObject.GetComponent<LocalPlayerScript>().getControllerID())) != 0)
+        {
+            nextStep = true;
+        }
+    }
+
+    void dAir()
+    {
+
+    }
+
+    void blink()
+    {
+
+    }
+
+    void reversal()
+    {
+
+    }
+
+    void super()
+    {
+
+    }
+
+    void displayLine(int i) {
+        text = tutorialText[i];
+        line++;
     }
 }
