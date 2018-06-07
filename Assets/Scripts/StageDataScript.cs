25ed4803a93661a474971357a9634107bcfa4702
rohinmeduri <rohinmeduri@gmail.com>
2018-06-06 19:35:30 -0700

    made basic stage select

---
+++ Assets/Scripts/StageDataScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageDataScript : MonoBehaviour {
+
+    public static bool created = false;
+    public Sprite stage;
+    // Use this for initialization
+
+    void Start()
+    {
+        if (created)
+        {
+            Destroy(gameObject);
+        }
+        created = true;
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/StageSelectScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageSelectScript : MonoBehaviour {
+    public Sprite stage;
+    public Image preview;
+
+	// Use this for initialization
+    public void stageClick()
+    {
+        preview.GetComponent<Image>().sprite = stage;
+        GameObject.Find("Stage Data Holder").GetComponent<StageDataScript>().stage = stage;
+    }
+}
