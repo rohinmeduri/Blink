1ca86b5743bcb601f87a41b994f3b936d98667c7
rohinmeduri <rohinmeduri@gmail.com>
2018-06-08 03:47:10 -0700

    added more comments'

--- Assets/Scripts/CameraShakeChanger.cs
+++ Assets/Scripts/CameraShakeChanger.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//this script is used by the camera shake option (a slider) in the pause menus and options menu - it changes the amount the camera shakes
 public class CameraShakeChanger : MonoBehaviour {
     private void Start()
     {
+        //sync value for each instance of the camera slider with the intensity that has already been set
         GetComponent<Slider>().value = GameObject.Find("Main Camera").GetComponent<CameraShake>().getIntensityMultiplier();
     }
+
+    //this function is called when the slider's value is changed
     public void changeIntensity(float multiplier)
     {
         GameObject.Find("Main Camera").GetComponent<CameraShake>().changeShakeIntensity(multiplier);
