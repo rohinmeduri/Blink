d8a3a7e6fa1886cba119f86737a24e06b16200ca
rohinmeduri <rohinmeduri@gmail.com>
2018-06-08 04:01:12 -0700

    more comments!

--- Assets/Scripts/DestroyLobby.cs
+++ Assets/Scripts/DestroyLobby.cs
@@ -5,16 +5,11 @@ using UnityEngine;
 public class DestroyLobby : MonoBehaviour {
 
     /**
-     * Used to destroy the lobby manager when going back to main lobby
+     * Used to destroy the lobby manager when going back to main lobby (because it is dontDestroyOnLoad
      */
 
 	// Use this for initialization
 	void Start () {
         Destroy(GameObject.Find("LobbyManager"));
     }
-
-    // Update is called once per frame
-    void Update () {
-		
-	}
 }
